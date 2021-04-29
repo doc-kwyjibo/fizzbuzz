6f75d6dfd94bb2dee42c27cdf9af40785b5c0f42
Daryl Kempthorne <daryl.kempthorne@gmail.com>
2021-04-29 17:54:56 +1000

    Move solution to another folder

---
+++ src/FizzBuzz.Library/IFizzBuzzResultCalculator.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FizzBuzz.Library
+{
+    public interface IFizzBuzzResultCalculator
+    {
+        string ComputeValue(int number);
+    }
+}
